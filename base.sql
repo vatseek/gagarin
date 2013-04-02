f92dd78e41acf247fc2e72397543244531e4fbf3
web8 <web8@do-m.do-m.net>
2013-04-03 00:06:48 +0300

    changed base

--- base.sql
+++ base.sql
@@ -4,6 +4,7 @@
 -- ------------------------------------------------------
 -- Server version	5.1.66-0+squeeze1
 
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
